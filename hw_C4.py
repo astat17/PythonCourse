eda958ce276b4b981e122940e7c48a62bb86a40a
astat17 <48257529+astat17@users.noreply.github.com>
2019-03-22 18:44:11 +0300

    Add files via upload

--- hw_C4.py
+++ hw_C4.py
@@ -1,6 +1,8 @@
 a, b, c = int(input('')), int(input('')), int(input(''))
-d = (b*b - 4*a*c)**0.5
-x1 = (-b - d)/2/a
-x2 = (-b + d)/2/a
-print(x1, x2)
-
+d = (b*b - 4*a*c)
+if d > 0 or d == 0:
+          x1 = (-b - d**0.5)/2/a
+          x2 = (-b + d**0.5)/2/a
+          print(x1, x2)
+else:
+          print('Корней нет')
